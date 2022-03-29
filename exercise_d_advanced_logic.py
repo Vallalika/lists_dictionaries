c9efce37024bfeef0548bb854f2737e4199d4eb7
Valerie Bayard <valerie.bayard@gmail.com>
2022-03-29 17:53:17 +0100

    first commit for exercise d

--- exercise_d_advanced_logic.py
+++ exercise_d_advanced_logic.py
@@ -1,15 +1,52 @@
 # For the following list of numbers:
 
+from logging import BufferingFormatter
+from operator import index
+from termios import OFDEL
+
+
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
 # 1. Print out a list of the even integers:
+even_list = []
+
+for number in numbers:
+    if number % 2 == 0:
+        even_list.append(number)
+
+print(even_list)
 
 
 # 2. Print the difference between the largest and smallest value:
-
+numbers.sort()
+diff = numbers[-1] - numbers[0]
+print(diff)
 
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
+# print("Exercise 3:")
+# print(numbers)
 
+# # for each number in the list
+# for number in numbers:
+#     print("My number is ", number)
+
+#     # track 2s
+#     temp = 0
+
+#     # if my number is 2 and temp is not 2:
+#     if number == 2 and temp != 2:
+#         print("entering number == 2 and temp != 2")
+
+#         # store number in temp variable
+#         temp = 2
+#         print("value of temp is", temp)
+
+#     # if my number is 2 and temp is also 2:
+#     elif number == 2 and temp == 2:
+
+#         # return True and break out of the loop
+#         print(True)
+#         break
 
 # 4. Print the sum of the numbers, 
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
